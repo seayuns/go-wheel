ace8c83a42c02a8b26cd88d62e9bb43a6cee8373
wuhaizhou <wuhaizhou@bingosoft.net>
2022-02-23 15:49:39 +0800

    timecache init

--- cache/timecache.go
+++ cache/timecache.go
@@ -1,1 +1,18 @@
 package cache
+
+import (
+	"sync"
+	"time"
+)
+
+type TimeCache struct {
+	*timecache
+}
+
+type timecache struct {
+	defaultExpiration time.Duration
+	items             sync.Map
+	mu                sync.Mutex
+	onEvicted         func(string, interface{}, bool)
+	// janitor           *janitor
+}
